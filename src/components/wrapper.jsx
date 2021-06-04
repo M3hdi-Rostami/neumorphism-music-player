96049f38dc44680bc1bb32fca67a5eecf99ac9de
m3hdi <m3hdi.rostami@gmail.com>
2021-06-04 21:00:50 +0430

    Add musics list and refactor all project

--- src/components/wrapper.jsx
+++ src/components/wrapper.jsx
@@ -8,12 +8,63 @@ import {
   faPlay,
   faPause,
   faFastForward,
+  faMusic,
 } from "@fortawesome/free-solid-svg-icons";
 import Button from "./button";
+import Copyright from "./copyright";
+import List from "./list";
 
 class Wrapper extends Component {
   state = {
-    musices: [
+    musics: [
+      {
+        id: 1,
+        title: "Asabani",
+        singer: "Shayea",
+        coverSrc: "/files/1.jpg",
+        musicSrc: "/files/1.mp3",
+        duration: "4:49",
+      },
+      {
+        id: 1,
+        title: "Asabani",
+        singer: "Shayea",
+        coverSrc: "/files/1.jpg",
+        musicSrc: "/files/1.mp3",
+        duration: "4:49",
+      },
+      {
+        id: 1,
+        title: "Asabani",
+        singer: "Shayea",
+        coverSrc: "/files/1.jpg",
+        musicSrc: "/files/1.mp3",
+        duration: "4:49",
+      },
+      {
+        id: 1,
+        title: "Asabani",
+        singer: "Shayea",
+        coverSrc: "/files/1.jpg",
+        musicSrc: "/files/1.mp3",
+        duration: "4:49",
+      },
+      {
+        id: 1,
+        title: "Asabani",
+        singer: "Shayea",
+        coverSrc: "/files/1.jpg",
+        musicSrc: "/files/1.mp3",
+        duration: "4:49",
+      },
+      {
+        id: 1,
+        title: "Asabani",
+        singer: "Shayea",
+        coverSrc: "/files/1.jpg",
+        musicSrc: "/files/1.mp3",
+        duration: "4:49",
+      },
       {
         id: 1,
         title: "Asabani",
@@ -32,11 +83,11 @@ class Wrapper extends Component {
       },
       {
         id: 3,
-        title: "Ghorse Ghamar2",
-        singer: "Behnam Bani",
+        title: "2JA",
+        singer: "Ho3ein & Bi Bal",
         coverSrc: "/files/3.jpg",
         musicSrc: "/files/3.mp3",
-        duration: "2:57",
+        duration: "4:03",
       },
       {
         id: 4,
@@ -64,89 +115,118 @@ class Wrapper extends Component {
       duration: "4:49",
     },
     playing: false,
-    likes: JSON.parse(localStorage.getItem("likes")) || [],
     likedClass: "",
+    showList: false,
   };
-componentDidMount(){
-  this.likeMount()
-}
-
+  componentDidMount() {
+    this.likeMount();
+  }
   render() {
+    const homePageClass = `w-100 flex_center_column ${
+      this.state.showList && "d_none"
+    }`;
+
     return (
       <div className="wrapper">
+        <header>
+          <Button
+            onClick={this.GoToHome}
+            icon={
+              this.state.showList ? (
+                <FontAwesomeIcon icon={faArrowLeft} />
+              ) : (
+                <FontAwesomeIcon icon={faMusic} />
+              )
+            }
+            cls="back"
+          />
+          <span className={this.state.playing ? "darkGreen" : "darkRed"}>
+            {this.state.playing ? "PLAY NOW" : "STOP NOW"}
+          </span>
+          <Button
+            onClick={this.showList}
+            icon={<FontAwesomeIcon icon={faBars} />}
+            cls="menu"
+          />
+        </header>
         <div className="main">
-          <header>
-            <Button icon={<FontAwesomeIcon icon={faArrowLeft} />} cls="back" />
-            <span className={this.state.playing ? "darkGreen" : "darkRed"}>
-              {this.state.playing ? "PLAY NOW" : "STOP NOW"}
-            </span>
-            <Button icon={<FontAwesomeIcon icon={faBars} />} cls="menu" />
-          </header>
-
-          <div className={`imageBox ${this.state.playing ? "playing" : ""}`}>
-            <img src={this.state.playingMusic.coverSrc} alt="" />
-          </div>
-          <div className="captionBox">
-            <h3>
-              {this.state.playingMusic.title} - {this.state.playingMusic.singer}
-            </h3>
-            <p>instagram: @m3hdi_v1</p>
-          </div>
-          <div className="truckBox">
-            <div className="timeBox">
-              <span>03:12</span>
-              <span>{this.state.playingMusic.duration}</span>
+          <div className={homePageClass}>
+            <div className={`imageBox ${this.state.playing ? "playing" : ""}`}>
+              <img src={this.state.playingMusic.coverSrc} alt="" />
             </div>
-            {/* <div className="truck"></div> */}
-            <audio src={this.state.playingMusic.musicSrc} id="song"></audio>
-            <input
-              type="range"
-              id="progressBar"
-              min="0"
-              max=""
-              value="0"
-              onChange={this.changeProgressBar}
-            />
+            <div className="captionBox">
+              <h3>
+                {this.state.playingMusic.title} -{" "}
+                {this.state.playingMusic.singer}
+              </h3>
+              <p>instagram: @m3hdi_v1</p>
+            </div>
+            <div className="truckBox">
+              <div className="timeBox">
+                <span>03:12</span>
+                <span>{this.state.playingMusic.duration}</span>
+              </div>
+              {/* <div className="truck"></div> */}
+              <audio src={this.state.playingMusic.musicSrc} id="song"></audio>
+              <input
+                type="range"
+                id="progressBar"
+                min="0"
+                max=""
+                value="0"
+                onChange={this.changeProgressBar}
+              />
+            </div>
+            <div className="controlBox">
+              <Button
+                onClick={this.like}
+                icon={<FontAwesomeIcon icon={faHeart} />}
+                cls="heart"
+                colorClass={this.state.likedClass}
+              />
+              <Button
+                icon={<FontAwesomeIcon icon={faFastBackward} />}
+                cls="backward"
+                onClick={this.previousSong}
+              />
+              <Button
+                icon={
+                  this.state.playing ? (
+                    <FontAwesomeIcon icon={faPause} />
+                  ) : (
+                    <FontAwesomeIcon icon={faPlay} />
+                  )
+                }
+                cls="play"
+                onClick={this.playPause}
+              />
+              <Button
+                icon={<FontAwesomeIcon icon={faFastForward} />}
+                cls="forward"
+                onClick={this.nextSong}
+              />
+            </div>
           </div>
-          <div className="controlBox">
-            <Button
-              onClick={this.like}
-              icon={<FontAwesomeIcon icon={faHeart} />}
-              cls="heart"
-              colorClass={this.state.likedClass}
-            />
-            <Button
-              icon={<FontAwesomeIcon icon={faFastBackward} />}
-              cls="backward"
-              onClick={this.previousSong}
-            />
-            <Button
-              icon={
-                this.state.playing ? (
-                  <FontAwesomeIcon icon={faPause} />
-                ) : (
-                  <FontAwesomeIcon icon={faPlay} />
-                )
-              }
-              cls="play"
-              onClick={this.playPause}
-            />
-            <Button
-              icon={<FontAwesomeIcon icon={faFastForward} />}
-              cls="forward"
-              onClick={this.nextSong}
-            />
-          </div>
+          <List
+            musics={this.state.musics}
+            playingMusic={this.state.playingMusic}
+            playPause={this.playOfList}
+            showList={this.state.showList}
+          />
+          <Copyright />
         </div>
       </div>
     );
   }
+  GoToHome = () => {
+    this.setState({ showList: false });
+  };
   like = () => {
-    let data = this.state.likes || [];
+    let data = JSON.parse(localStorage.getItem("likes")) || [];
     const index = data.indexOf(this.state.playingMusic.id);
     if (index > -1) {
       data.splice(index, 1);
-      this.setState({likedClass: ""});
+      this.setState({ likedClass: "" });
     } else {
       data.push(this.state.playingMusic.id);
       this.setState({ likedClass: "darkRed" });
@@ -154,21 +234,23 @@ componentDidMount(){
     data = [...new Set(data)];
     localStorage.setItem("likes", JSON.stringify(data));
   };
-
-  likeMount(id=1) {
-    let data = this.state.likes || [];
-    console.log(data);
-    console.log(id);
-    const index = data.indexOf(id);
-    console.log(index);
-      this.setState({ likedClass: (index > -1) ? "darkRed" : "" });
+  likeMount() {
+    let data = JSON.parse(localStorage.getItem("likes")) || [];
+    const index = data.indexOf(this.state.playingMusic.id);
+    this.setState({ likedClass: index > -1 ? "darkRed" : "" });
+  }
+  playOfList = async (id) => {
+    const music = this.state.musics.find((item) => item.id === id);
+    await this.setState({ playingMusic: music });
+    await this.setState({ playing: true });
+    document.getElementById("song").play();
+    this.updateProgressBar();
   };
-
   playPause = () => {
     const song = document.getElementById("song");
     if (this.state.playing) {
-      song.pause();
       this.setState({ playing: false });
+      song.pause();
     }
     if (!this.state.playing) {
       song.play();
@@ -177,27 +259,30 @@ componentDidMount(){
     this.updateProgressBar();
   };
   previousSong = async () => {
-    this.likeMount(this.state.playingMusic.id - 1);
     this.setPlayingMusicDuration();
 
     const prevMusicItem =
-      this.state.musices.find(
+      this.state.musics.find(
         (item) => item.id === this.state.playingMusic.id - 1
       ) ||
-      this.state.musices.find((item) => item.id === this.state.musices.length);
+      this.state.musics.find((item) => item.id === this.state.musics.length);
     await this.setState({ playingMusic: prevMusicItem });
     this.playPause();
+    this.likeMount();
   };
   nextSong = async () => {
-    this.likeMount(this.state.playingMusic.id + 1);
     this.setPlayingMusicDuration();
 
     const prevMusicItem =
-      this.state.musices.find(
+      this.state.musics.find(
         (item) => item.id === this.state.playingMusic.id + 1
-      ) || this.state.musices.find((item) => item.id === 1);
+      ) || this.state.musics.find((item) => item.id === 1);
     await this.setState({ playingMusic: prevMusicItem });
     this.playPause();
+    this.likeMount();
+  };
+  showList = () => {
+    this.setState({ showList: true });
   };
   changeProgressBar = () => {
     // this.updateProgressBar()
